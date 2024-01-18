9b1b9256b9ccb46258ec8bf427e8882d1248386c
unknown7761 <ismoilzanovnasrullo@gmail.com>
2024-01-18 18:07:38 +0500

    Initial Commit

---
+++ FlagGame/FlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGameApp.swift
+//  FlagGame
+//
+//  Created by Насрулло Исмоилжонов on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
